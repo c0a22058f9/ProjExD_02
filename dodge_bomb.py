588a13d0df07addb0c56013f5b785a5d2f438aa5
kudos <c0a22058f9@edu.teu.ac.jp>
2023-04-25 15:38:04 +0900

    練習問題5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,27 @@ delta = {
         pg.K_RIGHT: (+1, 0) # 右の移動量
         }
 
+
+def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool,bool]:
+    """
+    obj_rctがscr_rct内or外にあるのかを判定し、真理値タプルを返す関数
+    引数１：画面Surfaceのrect
+    引数２：オブジェクトのSurfaceのrect
+    戻り値：横方向、縦方向のはみだし判定結果（画面内：True/画面外：False）
+    """
+    yoko, tate = True, True
+
+    if obj_rct.left < scr_rct.left or obj_rct.right > scr_rct.right:
+        yoko = False
+    if obj_rct.top < scr_rct.top or obj_rct.bottom > scr_rct.bottom:
+        tate = False
+
+    return yoko, tate
+    
+
+    
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -19,8 +40,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_rect = kk_img.get_rect() # こうかとんのSurfaceに対するrectのSurfaceを取得
-    kk_rect.center = 900,400 # こうかとんの座標の中央を900, 400に設定
+    kk_rct = kk_img.get_rect() # こうかとんのSurfaceに対するrectのSurfaceを取得
+    kk_rct.center = 900,400 # こうかとんの座標の中央を900, 400に設定
     tmr = 0
 
     bb_img = pg.Surface((20,20)) # 爆弾のSurface
@@ -29,8 +50,8 @@ def main():
     x, y = random.randint(0,1600), random.randint(0,900) # 爆弾のランダムな座標を取得し、x, yに代入
     screen.blit(bb_img, [x, y]) # 爆弾を表示
     vx,vy = +1, +1 # 爆弾の移動用座標を作成
-    bb_rect = bb_img.get_rect() # 爆弾のSurfaceに対するrectのSurfaceを取得
-    bb_rect.center = x, y # 爆弾の中心座標をx, yに設定
+    bb_rct = bb_img.get_rect() # 爆弾のSurfaceに対するrectのSurfaceを取得
+    bb_rct.center = x, y # 爆弾の中心座標をx, yに設定
 
     while True:
         for event in pg.event.get():
@@ -42,13 +63,24 @@ def main():
         key_lst = pg.key.get_pressed() # 矢印キーの入力を受け付ける
         for k, mv in delta.items(): # 辞書deltaのkeyとvalueをk, mvに入れる
             if key_lst[k]: # 矢印キーの入力を受けたとき
-                kk_rect.move_ip(mv) # こうかとんの座標を移動
+                kk_rct.move_ip(mv) # こうかとんの座標を移動
+        
+        if check_bound(screen.get_rect(), kk_rct) != (True, True): # 画面内外判定
+            for k, mv in delta.items(): # 辞書deltaのkeyとvalueをk, mvに入れる
+                if key_lst[k]: # 矢印キーの入力を受けたとき
+                    kk_rct.move_ip(-mv[0], -mv[1]) # こうかとんの座標を移動。ひとつ前の値を入れたいため、それぞれ-mv[0], -mv[1]に設定
+
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, kk_rect)
+        screen.blit(kk_img, kk_rct)
         
-        bb_rect.move_ip(vx, vy) # 爆弾の座標を移動
-        screen.blit(bb_img, bb_rect) #  爆弾を移動後の座標に表示
+        bb_rct.move_ip(vx, vy) # 爆弾の座標を移動
+        yoko, tate = check_bound(screen.get_rect(), bb_rct)
+        if not yoko: # 横方向にはみ出ていたら
+            vx *= -1 # 横方向の移動を反転
+        if not tate: # 縦方向にはみ出ていたら
+            vy *= -1 # 縦方向の移動を反転
+        screen.blit(bb_img, bb_rct) #  爆弾を移動後の座標に表示
 
         pg.display.update()
         clock.tick(1000)

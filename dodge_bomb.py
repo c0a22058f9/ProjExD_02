d1045ba2fbaa83eee3a8ffba92d3c79ae7c1e005
kudos <c0a22058f9@edu.teu.ac.jp>
2023-04-25 14:21:06 +0900

    練習問題2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,7 @@
-import pygame as pg
+import random
 import sys
+
+import pygame as pg
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,6 +14,7 @@ def main():
     bb_img = pg.Surface((20,20)) # 爆弾のSurface
     pg.draw.circle(bb_img,(255,0,0),(10,10),10) # 爆弾を作成
     bb_img.set_colorkey((0,0,0))  # 爆弾の余白を透明化
+    x, y = random.randint(0,1600), random.randint(0,900) # 爆弾のランダムな座標を取得
 
     while True:
         for event in pg.event.get():
@@ -21,6 +24,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_img,[x,y])
 
         pg.display.update()
         clock.tick(1000)

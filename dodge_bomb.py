20a2a11bd6cddf68fdb576981cd98345256af751
kudos <c0a22058f9@edu.teu.ac.jp>
2023-04-25 14:34:33 +0900

    練習問題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -15,6 +15,10 @@ def main():
     pg.draw.circle(bb_img,(255,0,0),(10,10),10) # 爆弾を作成
     bb_img.set_colorkey((0,0,0))  # 爆弾の余白を透明化
     x, y = random.randint(0,1600), random.randint(0,900) # 爆弾のランダムな座標を取得
+    screen.blit(bb_img, [x, y])
+    vx,vy = +1, +1 # 爆弾の移動用座標を作成
+    bb_rect = bb_img.get_rect() # 爆弾のSurfaceに対するrectのSurfaceを取得
+    bb_rect.center = x, y # 爆弾の中心座標をx, yに設定
 
     while True:
         for event in pg.event.get():
@@ -24,7 +28,9 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img,[x,y])
+        
+        bb_rect.move_ip(vx, vy) # 爆弾の座標を移動
+        screen.blit(bb_img, bb_rect) #  爆弾を移動後の座標に表示
 
         pg.display.update()
         clock.tick(1000)
